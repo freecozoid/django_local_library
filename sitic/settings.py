c1c1fe09b9c7fe4f7457af9908b83ade60a5e30d
freecozoid <lllep6ajlb@yandex.ru>
2018-11-06 15:35:48 +0300

    Load polls

--- sitic/settings.py
+++ sitic/settings.py
@@ -123,6 +123,7 @@ LOGIN_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
